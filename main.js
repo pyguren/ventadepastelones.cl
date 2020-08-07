cd285d85b30d71560f01a1882fefecf5b5486738
seoguren <estebansologuren@gmail.com>
2020-08-07 18:58:59 -0400

    agregando catalogo de productos

--- main.js
+++ main.js
@@ -1,32 +1,28 @@
-document.getElementById("inicio").style.display = "block";
-document.getElementById("servicios").style.display = "none";
-document.getElementById("productos").style.display = "none";
-document.getElementById("contacto").style.display = "none";
 
-function inicio() {
-  document.getElementById("inicio").style.display = "block";
-  document.getElementById("servicios").style.display = "none";
-  document.getElementById("productos").style.display = "none";
-  document.getElementById("contacto").style.display = "none";
-};
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function servicios() {
-  document.getElementById("inicio").style.display = "none";
-  document.getElementById("servicios").style.display = "block";
-  document.getElementById("productos").style.display = "none";
-  document.getElementById("contacto").style.display = "none";
-};
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-function productos() {
-  document.getElementById("inicio").style.display = "none";
-  document.getElementById("servicios").style.display = "none";
-  document.getElementById("productos").style.display = "block";
-  document.getElementById("contacto").style.display = "none";
-};
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-function contacto() {
-  document.getElementById("inicio").style.display = "none";
-  document.getElementById("servicios").style.display = "none";
-  document.getElementById("productos").style.display = "none";
-  document.getElementById("contacto").style.display = "block";
-};
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
